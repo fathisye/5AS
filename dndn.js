a6cb6978b45e8c88a88ebc96bdda5f34ef5b360a
fathisye <47281515+fathisye@users.noreply.github.com>
2019-02-09 16:33:27 +0200

    Add files via upload

--- dndn.js
+++ dndn.js
@@ -1,56 +1,20 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
+console.log("Scrpit By Dream");
 
-console.log("Welcome Again !");
- 
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
 
-‏نقدم لك دعوة لسموك الراقيَ .
-‏لنحظى بجمال حضورك وتواجدك
-•بتنـورونـا الجميع :wink:
-•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
-•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
-•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
-شرفنا يا عسل:wink: :kissing_heart:
-
-https://discord.gg/nGtWfY
-joined info-selfie
-! Selfie.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
-}).catch(console.error)
-
+client.on("ready", () => {
+let channel =     client.channels.get("542328854199664661")
+setInterval(function() {
+channel.send(`spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspam`);
+}, 30)
 })
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
 
-‏نقدم لك دعوة لسموك الراقيَ .
-‏لنحظى بجمال حضورك وتواجدك
-•بتنـورونـا الجميع :wink:
-•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
-•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
-•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
-شرفنا يا عسل:wink: :kissing_heart:
-
-https://discord.gg/nGtWfY
-joined info-selfie
-! Selfie.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`) 
-}).catch(console.error)
-})
 client.login(process.env.BOT_TOKEN);
